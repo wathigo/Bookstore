acf7b2eb2b690032a73f922b10272ff8dcfbf0db
samuel <samuelgarciacompanys@gmail.com>
2019-12-09 17:16:29 +0100

    linters

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,12 +3,12 @@ const Actions = (() => {
   const CREATE_BOOK = 'CREATE_BOOK';
   const REMOVE_BOOK = 'REMOVE_BOOK';
 
-  const createBook = book => ({
+  const createBook = (book) => ({
     type: CREATE_BOOK,
     book,
   });
 
-  const removeBook = book => ({
+  const removeBook = (book) => ({
     type: REMOVE_BOOK,
     book_id: book.id,
   });
